5268ab5a68fbc58cae4c2360e6f5ce93033c275c
abhilash.venugopal <abhilash.venugopal.112299@gmail.com>
2025-06-11 17:36:14 +0530

    Practice on Creating a simple Chat-Bot using Python.

--- CHAT BOT DIR/math.py
+++ CHAT BOT DIR/math.py
@@ -1,3 +1,7 @@
 from chatterbot import ChatBot
 
 bot =ChatBot("Math", logic_adapters=["chatterbot.logic.MathematicalEvaluation"])
+
+while True:
+    user_text = input("type the math eqaution you want to solve: ")
+    print(f"Chatbot: {str(bot.get_response(user_text))}")
